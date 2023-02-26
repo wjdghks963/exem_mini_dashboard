18cb66ed7a58a139ffa5620cbf42a95d72fafb9a
wjdghks963 <chsw000@naver.com>
2023-02-26 13:02:48 +0900

    added :  - Line Chart 컴포넌트

---
+++ src/components/Charts/LineChart.tsx
@@ -0,0 +1,57 @@
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import {useQuery} from "react-query";
+import {getData} from "../../libs/getData";
+import {fromUnixTime, toUnixTime} from "../../libs/unixtimeConverter";
+import { TimeSeriesResponse} from "../../type";
+import {ChartLayout} from "../ChartLayout";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+
+export function LineChart({selectedTime}:{selectedTime:number}) {
+
+    const now = toUnixTime(new Date());
+
+    const {data:chartData, isLoading, error} = useQuery(['lineChartData', selectedTime], ()=>getData<TimeSeriesResponse>(`/timeseries?from=${((now-selectedTime)*1000)}&to=${now*1000}`),{refetchInterval: 10000 });
+
+    const data = {
+        labels: chartData?.times.map((time:number) => fromUnixTime(time)),
+        datasets: [
+            {
+                label: chartData?.name,
+                data: chartData?.data.map((label:number) => String(label)),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            },
+
+        ]
+    };
+
+
+    return (
+       <ChartLayout isLoading={isLoading} error={error}>
+           <Line data={data} />
+       </ChartLayout>
+    );
+}
+
+
