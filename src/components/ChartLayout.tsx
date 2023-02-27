0a96947a0c588cc8ec9506e86b2811145bb35637
wjdghks963 <chsw000@naver.com>
2023-02-27 17:00:19 +0900

    added :  - 차트 resize 가능

--- src/components/ChartLayout.tsx
+++ src/components/ChartLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Loading} from "./Loading";
 import {Error} from "./Error";
+import styled from "styled-components";
 
 interface ChartLayout {
     children:React.ReactNode
@@ -12,5 +13,10 @@ export function ChartLayout({children, isLoading, error}:ChartLayout){
 
 if(isLoading) return <Loading/>
 if(error) return <Error message={"에러가 일어났습니다."}/>
-return <div>{children}</div>
+return <Container>{children}</Container>
 }
+
+const Container = styled.div`
+  resize: both;
+  overflow: auto;
+`
