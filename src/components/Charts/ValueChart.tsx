9eba78314accd0ff62e3d3edd37a01c8dfe3a72b
wjdghks963 <chsw000@naver.com>
2023-02-28 18:17:23 +0900

    added :  - 소수점 기능  - 일정값 이상일때 빨간색으로 표시

--- src/components/Charts/ValueChart.tsx
+++ src/components/Charts/ValueChart.tsx
@@ -5,6 +5,7 @@ import {ValueResponse} from "../../type";
 import {ChartLayout} from "../ChartLayout";
 import styled from "styled-components";
 
+const criticalPoint = 4;
 
 export function ValueChart({selectedTime}:{selectedTime:number}) {
 
@@ -12,17 +13,28 @@ export function ValueChart({selectedTime}:{selectedTime:number}) {
 
     const {data:chartData, isLoading, error} = useQuery(['valueChartData',selectedTime], ()=>getData<ValueResponse>(`/value?from=${(now-(selectedTime))*1000}&to=${now*1000}`),{refetchInterval: 10000 });
 
+    const byteToKb = (byte:number|undefined):string =>{
+        const kb = ((byte) ? (byte) / (1024) : 0).toFixed(2).toString();
+        const kbArr = kb.split(".");
+        const integer = parseInt(kbArr[0].slice(-3)).toString();
+        const decimal = kbArr[1];
+        return `${integer}.${decimal}`;
+    }
+
+    const value = byteToKb(chartData?.value);
+
     return (
-        <ChartLayout isLoading={isLoading} error={error}>
-            <ValueContainer>
-                {chartData?.value}
+        <ChartLayout  isLoading={isLoading} error={error}>
+            <ValueContainer value={value} criticalPoint={criticalPoint}>
+                {value}
             </ValueContainer>
         </ChartLayout>
     );
 }
 
-const ValueContainer = styled.div`
+const ValueContainer = styled.div<{ criticalPoint: number, value:string }>`
   display: flex;
+  width: 100%;
   padding: 20px 0;
   border: 1px solid silver;
   border-radius: 5px;
@@ -30,9 +42,10 @@ const ValueContainer = styled.div`
   font-weight: bold;
   font-size: 34px;
   align-items: end;
+  color: ${props => +props.value > props.criticalPoint ? "red" : "black"  };
   
   &::after {
-    content: "Mib";
+    content: "Kb";
     font-weight: 500;
     font-size: 20px;
   }
